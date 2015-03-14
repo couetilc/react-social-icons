6cf11c1149a4c163df71349dd8733367d0b46032
jaketrent <trent.jake@gmail.com>
2015-03-14 08:27:23 -0600

    better names for internal component classes

--- examples/icon/app.js
+++ examples/icon/app.js
@@ -1,9 +1,22 @@
 var React = require('react');
 
+var networks = require('../../lib/networks');
 var SocialIcon = require('../../lib/social-icon');
 
-var separateIcons = (
-  <SocialIcon url={"http://pinterest.com/jake"} />
+var sizes = (
+  <div>
+    <SocialIcon network="pinterest" className="sm" />
+    <SocialIcon network="pinterest" className="med" />
+    <SocialIcon network="pinterest" className="lrg" />
+    <SocialIcon network="pinterest" className="xlrg" />
+  </div>
 );
 
-React.render(separateIcons, document.getElementById('icon'));
+var lib = (
+  <div>
+    {networks.KEYS.map((k) => { return <SocialIcon network={k} />; })}
+  </div>
+)
+
+React.render(sizes, document.getElementById('sizes'));
+React.render(lib, document.getElementById('lib'));
