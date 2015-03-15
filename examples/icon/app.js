74b3c76eb799cf38b8c572fefa7f09788273c40d
jaketrent <trent.jake@gmail.com>
2015-03-14 21:18:25 -0600

    let color override

--- examples/icon/app.js
+++ examples/icon/app.js
@@ -16,17 +16,22 @@ var lib = (
   <div>
     {networks.KEYS.map((k) => { return <SocialIcon network={k} title={k} key={k} />; })}
   </div>
-)
+);
 
 var urlExample = (
   <SocialIcon url="http://linkedin.com/in/jaketrent" />
-)
+);
 
 var networkExample = (
   <SocialIcon network="tumblr" url="http://jaketrent.com" />
-)
+);
+
+var colorExample = (
+  <SocialIcon network="pinterest" color="#ff5a01" />
+);
 
 React.render(sizes, document.getElementById('sizes'));
 React.render(lib, document.getElementById('lib'));
 React.render(networkExample, document.getElementById('network-example'));
 React.render(urlExample, document.getElementById('url-example'));
+React.render(colorExample, document.getElementById('color-example'));
