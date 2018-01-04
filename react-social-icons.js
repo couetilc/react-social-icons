e1587773a9bac4eb988bcb40f378b0533e6b92cd
jaketrent <trent.jake@gmail.com>
2018-01-04 06:55:17 -0700

    Release v2.8.0

---
+++ react-social-icons.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.__esModule = true;
+exports.default = exports.SocialIcons = exports.SocialIcon = undefined;
+
+var _socialIcon = require('./social-icon');
+
+var _socialIcon2 = _interopRequireDefault(_socialIcon);
+
+var _socialIcons = require('./social-icons');
+
+var _socialIcons2 = _interopRequireDefault(_socialIcons);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.SocialIcon = _socialIcon2.default;
+exports.SocialIcons = _socialIcons2.default;
+exports.default = _socialIcons2.default;
