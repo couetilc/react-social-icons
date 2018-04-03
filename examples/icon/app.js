e78575a5e466d39637e1ed4022cc48b0b897a513
jaketrent <trent.jake@gmail.com>
2018-04-03 08:09:15 -0600

    refactor: remove SocialIcons
    SocialIcon can accomplish everything; simplifies api; gets rid of a couple
    troublesome scenarios
    
    BREAKING CHANGE SocialIcons component removed

--- examples/icon/app.js
+++ examples/icon/app.js
@@ -1,46 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as networks from '../../src/networks';
-import SocialIcon from '../../src/social-icon';
-import SocialIcons from '../../src/social-icons';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as networks from '../../src/networks'
+import SocialIcon from '../../src/social-icon'
 
 const lib = (
   <div>
-    {networks.KEYS.map((k) => (<SocialIcon network={k} title={k} key={k} />))}
+    {networks.KEYS.map(k => <SocialIcon network={k} title={k} key={k} />)}
   </div>
-);
+)
 
-const urlExample = (
-  <SocialIcon url="http://linkedin.com/in/jaketrent" />
-);
+const urlExample = <SocialIcon url="http://linkedin.com/in/jaketrent" />
 
 const networkExample = (
   <SocialIcon network="tumblr" url="http://jaketrent.com" />
-);
+)
 
-const colorExample = (
-  <SocialIcon network="twitter" color="#ff5a01" />
-);
+const colorExample = <SocialIcon network="twitter" color="#ff5a01" />
 
 const labelExample = (
   <SocialIcon url="https://www.example.com" label="Our portfolio" />
-);
-
-const urls = [
-  'http://jaketrent.com',
-  'http://twitter.com/jaketrent',
-  'http://linkedin.com/in/jaketrent',
-  'https://www.pinterest.com/jaketrent/artsy-fartsy/',
-  'mailto:awesome@jaketrent.com',
-];
-
-const iconsExample = (
-  <SocialIcons urls={urls} className="med" />
-);
-
-const iconsColorExample = (
-  <SocialIcons urls={urls} color="black" className="sm" />
-);
+)
 
 const sizes = (
   <div>
@@ -49,13 +28,11 @@ const sizes = (
     <SocialIcon network="pinterest" style={{ height: 100, width: 100 }} />
     <SocialIcon network="pinterest" style={{ height: 200, width: 200 }} />
   </div>
-);
+)
 
-ReactDOM.render(lib, document.getElementById('lib'));
-ReactDOM.render(networkExample, document.getElementById('network-example'));
-ReactDOM.render(urlExample, document.getElementById('url-example'));
-ReactDOM.render(colorExample, document.getElementById('color-example'));
-ReactDOM.render(labelExample, document.getElementById('label-example'));
-ReactDOM.render(iconsExample, document.getElementById('icons-example'));
-ReactDOM.render(iconsColorExample, document.getElementById('icons-color-example'));
-ReactDOM.render(sizes, document.getElementById('sizes'));
+ReactDOM.render(lib, document.getElementById('lib'))
+ReactDOM.render(networkExample, document.getElementById('network-example'))
+ReactDOM.render(urlExample, document.getElementById('url-example'))
+ReactDOM.render(colorExample, document.getElementById('color-example'))
+ReactDOM.render(labelExample, document.getElementById('label-example'))
+ReactDOM.render(sizes, document.getElementById('sizes'))
--- src/react-social-icons.js
+++ src/react-social-icons.js
@@ -1,3 +1,1 @@
-export SocialIcon from './social-icon';
-export SocialIcons from './social-icons';
-export default from './social-icons';
+export SocialIcon from './social-icon'
