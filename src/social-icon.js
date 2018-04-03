d3f815db4035316fdb66ec2856f8274873b69a97
Elias Mason <eliasjmason@gmail.com>
2018-04-02 18:09:45 -0700

    Use networkKey as fallback label if no label provided.

--- src/social-icon.js
+++ src/social-icon.js
@@ -21,7 +21,7 @@ function SocialIcon(props) {
        rel="noopener"
        className={cx('social-icon', className)}
        style={{ ...socialIcon, ...props.style }}
-       aria-label={label}>
+       aria-label={label || networkKey}>
       <div className="social-container" style={socialContainer} >
         <svg className="social-svg" style={socialSvg} viewBox="0 0 64 64">
           <Background />
