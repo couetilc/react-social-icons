ba7bd446f4cdf8b61b4f5b72e49d6935f157032c
Connor Couetil <couetilc@gmail.com>
2022-02-03 21:12:50 -0800

    i have a build working, now need to test it

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   build: {
     target: 'es2015',
+    lib: {
+      entry: './src/react-social-icons.js',
+      name: "SocialIcon",
+      fileName: format => `react-social-icons.${format}.js`,
+    },
+    rollupOptions: {
+      // Check if this actually does something?
+      external: ['react', 'react-dom'],
+      // Do I need this?
+      output: {
+        globals: {
+          react: 'React',
+        }
+      },
+    },
   },
   plugins: [
     react(),
