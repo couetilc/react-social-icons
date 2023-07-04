741f17f6aa73b0797edd7c4e0ac0ebbfb24b6665
Connor Couetil <couetilc@gmail.com>
2023-07-03 22:36:00 -0400

    run unit tests on emitted bundles

--- README.md
+++ README.md
@@ -43,8 +43,6 @@ To open a link in a new tab, pass the `target` prop to `<SocialIcon>`. The `<Soc
 
 # TODO
 
-- need to rune a sanity check on outputted bundles
-
 - can I test for code splitting yet?
 
 - update all dependencies, dev and not dev
---
+++ test/unit/dist.test.js
@@ -0,0 +1,8 @@
+import { cases } from './cases.js'
+import { SocialIcon as EsmSocialIcon } from '../../dist/react-social-icons.js'
+import { SocialIcon as ComponentSocialIcon } from '../../dist/component.js'
+import { SocialIcon as CjsSocialIcon } from '../../dist/react-social-icons.cjs'
+
+cases(EsmSocialIcon)
+cases(ComponentSocialIcon)
+cases(CjsSocialIcon)
